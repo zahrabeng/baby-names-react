3c2c83d88c1effad52ca6679a7f38f01bc636543
Zahra <bghzahra@outlook.com>
2022-03-17 20:58:55 +0000

    added css to page

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,8 +1,32 @@
-//import allBabyNames from "../babynames.json"
-import arrOfNames from "./reorderNames"
+import allBabyNames from "../babynames.json"
+import sorting from "./reorderNames";
+import { babyNameInfo } from "./reorderNames";
+import { useState } from "react"
+
+ 
 
 export default function Main():JSX.Element{
- 
+    const babyNames:babyNameInfo[] = [...allBabyNames]
+
+    const [searchText, setSearchText] = useState('');
+
+    //sorting names by alphabetical order using imported sorting function
+    const sortedNames = babyNames.sort(sorting)
+
+    //mapping list of each name in JSON database
+    function arrOfNames():JSX.Element[]{
+        const allNames = filteredBabyNames.map((eachName)=> 
+        <button key={eachName.id} className={eachName.sex}>{eachName.name}</button>)
+        return allNames
+    }
+
+    //filters baby names in search bar to whatever is typed in
+    const filteredBabyNames = sortedNames.filter((oneBabyName:babyNameInfo) => oneBabyName.name.toLowerCase().includes(searchText))
+
+
+    function handleSearchText(e:any){
+        setSearchText(e.target.value.toLowerCase())
+    }
 
 
     return(
@@ -11,9 +35,13 @@ export default function Main():JSX.Element{
             <h1>Baby Names</h1>
         </header>
         <main>
-            <input placeholder="search name ..."/>
+            <input 
+            placeholder="search name ..." 
+            value={searchText}
+            onChange={handleSearchText}
+            />  
         <hr/>
-            {arrOfNames()} 
+            {arrOfNames()}
         </main>
 
         </>
