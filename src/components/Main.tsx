262cec6e98e17ad6295b3b6b50816f5779badc3a
Zahra <bghzahra@outlook.com>
2022-03-18 02:54:58 +0000

    added some code to make names go to favourites list, still not working how it should

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -9,31 +9,43 @@ export default function Main():JSX.Element{
     const babyNames:babyNameInfo[] = [...allBabyNames]
 
     const [searchText, setSearchText] = useState('');
+    const [favourites, setFavourites] = useState<any>([]);
 
     //sorting names by alphabetical order using imported sorting function
     const sortedNames = babyNames.sort(sorting)
 
+     //filters baby names in search bar to whatever is typed in
+     const filteredBabyNames = sortedNames.filter((oneBabyName:babyNameInfo) => oneBabyName.name.toLowerCase().includes(searchText))
+
     //mapping list of each name in JSON database
     function arrOfNames():JSX.Element[]{
         const allNames = filteredBabyNames.map((eachName)=> 
-        <button key={eachName.id} className={eachName.sex}>{eachName.name}</button>)
+        <button key={eachName.id} className={eachName.sex} value={eachName.name} 
+        onClick = {handleClick}>
+        {eachName.name}
+        </button>)
         return allNames
     }
 
-    //filters baby names in search bar to whatever is typed in
-    const filteredBabyNames = sortedNames.filter((oneBabyName:babyNameInfo) => oneBabyName.name.toLowerCase().includes(searchText))
+    const handleClick = ((e:any) => setFavourites([...favourites, e.target.value]))
 
 
     function handleSearchText(e:any){
         setSearchText(e.target.value.toLowerCase())
     }
 
+
+    const favouritesArray = favourites.map((faveName:string) =>
+    <button key={faveName}>
+        {faveName}
+    </button>
+    )
+
     function handleZero(){
         if (filteredBabyNames.length === 0){
             return <p className="zero">Sorry! No names matching this search :(</p>
         }
     }
-
 
     return(
         <>
@@ -46,6 +58,11 @@ export default function Main():JSX.Element{
             value={searchText}
             onChange={handleSearchText}
             />  
+        <div className="favourites">
+            <p>List of Favourites:</p>
+            {favouritesArray}
+        </div>
+        <hr/> 
         <div className="babyNames">
             {arrOfNames()}
             {handleZero()}
