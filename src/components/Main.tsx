a24ad7202bb302d57a652c72669f76c67cb978aa
Zahra <bghzahra@outlook.com>
2022-03-18 18:58:35 +0000

    fixed favourites bar bug still need to remove the names added up to favourites from main list on click

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -9,38 +9,46 @@ export default function Main():JSX.Element{
     const babyNames:babyNameInfo[] = [...allBabyNames]
 
     const [searchText, setSearchText] = useState('');
-    const [favourites, setFavourites] = useState<any>([]);
+    const [favourites, setFavourites] = useState<babyNameInfo[]>([]);
 
     //sorting names by alphabetical order using imported sorting function
-    const sortedNames = babyNames.sort(sorting)
+    const sortedNames = babyNames.sort(sorting);
 
      //filters baby names in search bar to whatever is typed in
-     const filteredBabyNames = sortedNames.filter((oneBabyName:babyNameInfo) => oneBabyName.name.toLowerCase().includes(searchText))
+     const filteredBabyNames = sortedNames.filter((oneBabyName:babyNameInfo) => 
+     oneBabyName.name.toLowerCase().includes(searchText));
 
     //mapping list of each name in JSON database
     function arrOfNames():JSX.Element[]{
-        const allNames = filteredBabyNames.map((eachName)=> 
-        <button key={eachName.id} className={eachName.sex} value={eachName.name} 
-        onClick = {handleClick}>
+        const allNames = filteredBabyNames.map((eachName)=>
+        <button key={eachName.id} className={eachName.sex} 
+        value={eachName.name} 
+        onClick ={()=> setFavourites([...favourites,eachName])}>
         {eachName.name}
         </button>)
         return allNames
     }
-
-    const handleClick = ((e:any) => setFavourites([...favourites, e.target.value]))
-
-
+ 
     function handleSearchText(e:any){
         setSearchText(e.target.value.toLowerCase())
     }
 
 
-    const favouritesArray = favourites.map((faveName:string) =>
-    <button key={faveName}>
-        {faveName}
-    </button>
+    const favouritesArray = favourites.map((faveName:babyNameInfo) =>
+    <button key={faveName.id} className={faveName.sex}>{faveName.name}</button>
     )
 
+    // function isInList(){
+    //     for (const name of favourites){
+    //         if (filteredBabyNames.includes(name)){
+    //             const index = filteredBabyNames.indexOf(name)
+    //             filteredBabyNames.splice(index,1)
+    //         }
+    //     }return filteredBabyNames
+    // }
+
+
+    //prints message when there are no names matching the search
     function handleZero(){
         if (filteredBabyNames.length === 0){
             return <p className="zero">Sorry! No names matching this search :(</p>
@@ -66,6 +74,7 @@ export default function Main():JSX.Element{
         <div className="babyNames">
             {arrOfNames()}
             {handleZero()}
+            {console.log(favourites)}
         </div>
         </main>
 
