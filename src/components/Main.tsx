73ff637e28709cba5e8f8b0a16b84123a390666a
Zahra <bghzahra@outlook.com>
2022-06-16 11:26:25 +0100

    added p tag to show count of names that appear when searched

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,84 +1,91 @@
-import allBabyNames from "../babynames.json"
+import allBabyNames from "../babynames.json";
 import sorting from "./reorderNames";
 import { babyNameInfo } from "./reorderNames";
-import { useState } from "react"
+import { useState } from "react";
 
- 
+export default function Main(): JSX.Element {
+  const babyNames: babyNameInfo[] = [...allBabyNames];
 
-export default function Main():JSX.Element{
-    const babyNames:babyNameInfo[] = [...allBabyNames]
+  const [searchText, setSearchText] = useState("");
+  const [favourites, setFavourites] = useState<babyNameInfo[]>([]);
 
-    const [searchText, setSearchText] = useState('');
-    const [favourites, setFavourites] = useState<babyNameInfo[]>([]);
+  //sorting names by alphabetical order using imported sorting function
+  const sortedNames = babyNames.sort(sorting);
 
-    //sorting names by alphabetical order using imported sorting function
-    const sortedNames = babyNames.sort(sorting);
+  //filters baby names in search bar to whatever is typed in
+  const filteredBabyNames = sortedNames.filter((oneBabyName: babyNameInfo) =>
+    oneBabyName.name.toLowerCase().includes(searchText)
+  );
 
-     //filters baby names in search bar to whatever is typed in
-     const filteredBabyNames = sortedNames.filter((oneBabyName:babyNameInfo) => 
-     oneBabyName.name.toLowerCase().includes(searchText));
-
-    //mapping list of each name in JSON database
-    function arrOfNames():JSX.Element[]{
-        const allNames = filteredBabyNames.map((eachName)=>
-        <button key={eachName.id} className={eachName.sex} 
-        value={eachName.name} 
-        onClick ={()=> setFavourites([...favourites,eachName])}>
+  //mapping list of each name in JSON database
+  function arrOfNames(): JSX.Element[] {
+    const allNames = filteredBabyNames.map((eachName) => (
+      <button
+        key={eachName.id}
+        className={eachName.sex}
+        value={eachName.name}
+        onClick={() => setFavourites([...favourites, eachName])}
+      >
         {eachName.name}
-        </button>)
-        return allNames
+      </button>
+    ));
+    return allNames;
+  }
+
+  function handleSearchText(e: any) {
+    setSearchText(e.target.value.toLowerCase());
+  }
+
+  const favouritesArray = favourites.map((faveName: babyNameInfo) => (
+    <button key={faveName.id} className={faveName.sex}>
+      {faveName.name}
+    </button>
+  ));
+
+  // function isInList(){
+  //     for (const name of favourites){
+  //         if (filteredBabyNames.includes(name)){
+  //             const index = filteredBabyNames.indexOf(name)
+  //             filteredBabyNames.splice(index,1)
+  //         }
+  //     }return filteredBabyNames
+  // }
+
+  //prints message when there are no names matching the search
+  function handleZero() {
+    if (filteredBabyNames.length === 0) {
+      return <p className="zero">Sorry! No names matching this search :(</p>;
     }
- 
-    function handleSearchText(e:any){
-        setSearchText(e.target.value.toLowerCase())
-    }
+  }
 
-
-    const favouritesArray = favourites.map((faveName:babyNameInfo) =>
-    <button key={faveName.id} className={faveName.sex}>{faveName.name}</button>
-    )
-
-    // function isInList(){
-    //     for (const name of favourites){
-    //         if (filteredBabyNames.includes(name)){
-    //             const index = filteredBabyNames.indexOf(name)
-    //             filteredBabyNames.splice(index,1)
-    //         }
-    //     }return filteredBabyNames
-    // }
-
-
-    //prints message when there are no names matching the search
-    function handleZero(){
-        if (filteredBabyNames.length === 0){
-            return <p className="zero">Sorry! No names matching this search :(</p>
-        }
-    }
-
-    return(
-        <>
-        <header>
-            <h1>Baby Names App</h1>
-        </header>
-        <main>
-            <input 
-            placeholder="search name ..." 
-            value={searchText}
-            onChange={handleSearchText}
-            />  
+  return (
+    <>
+      <header>
+        <h1>Baby Names App</h1>
+      </header>
+      <main>
+        <input
+          placeholder="search name ..."
+          value={searchText}
+          onChange={handleSearchText}
+        />
+        <div>
+          <p className="names-length">
+            {" "}
+            Showing {filteredBabyNames.length} names out of {sortedNames.length}
+          </p>
+        </div>
         <div className="favourites">
-            <p>List of Favourites:</p>
-            {favouritesArray}
+          <p>List of Favourites:</p>
+          {favouritesArray}
         </div>
-        <hr/> 
+        <hr />
         <div className="babyNames">
-            {arrOfNames()}
-            {handleZero()}
-            {console.log(favourites)}
+          {arrOfNames()}
+          {handleZero()}
+          {console.log(favourites)}
         </div>
-        </main>
-
-        </>
-
-    )
+      </main>
+    </>
+  );
 }
--- src/components/reorderNames.tsx
+++ src/components/reorderNames.tsx
@@ -1,21 +1,15 @@
-
 export interface babyNameInfo {
-    id: number;
-    name:string;
-    sex: string;
+  id: number;
+  name: string;
+  sex: string;
 }
-
 
 // comparing two names and ordering them based on alphabetical value
-export default function sorting (a:babyNameInfo, b:babyNameInfo):number{
-    if(a.name < b.name){
-        return -1
-    }
-    else if(a.name > b.name){
-        return 1
-    }return 0;
+export default function sorting(a: babyNameInfo, b: babyNameInfo): number {
+  if (a.name < b.name) {
+    return -1;
+  } else if (a.name > b.name) {
+    return 1;
+  }
+  return 0;
 }
-
-
-
-
